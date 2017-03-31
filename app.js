5e6af2fd8905bbdf88befa4fa681ea4f87103002
James Runham <james.runham@reedonline.co.uk>
2017-03-31 15:47:57 +0100

    set up handlebars

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const PORT = 3000;
 
 var express = require('express');
+var handlebars = require('express3-handlebars');
+var path = require('path');
+
 var app = express();
 
+// set up templating
+app.engine('handlebars', handlebars({
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res){
-    res.render('');
+    res.render('index');
 });
 
 app.listen(PORT, function(){
