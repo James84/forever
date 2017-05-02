9730029792a41e5554bfaf495c17ef2b7169d1b7
James Runham <james.runham@reedonline.co.uk>
2017-05-02 15:01:48 +0100

    set up partial

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // set up templating
 app.engine('handlebars', handlebars({
     layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialsDir: path.join(__dirname, '/views/partials/'),
     defaultLayout: 'main',
     helpers: {
             section: function (name, options) {
@@ -20,7 +21,7 @@ app.engine('handlebars', handlebars({
                 this._sections[name] = options.fn(this);
                 return null;
             }
-        } 
+        }
 }));
 
 app.set('view engine', 'handlebars');
@@ -29,13 +30,32 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
-app.get('/product/:id', function(req, res){
-   res.render('view'); 
+app.get('/crafts', function(req, res){
+    res.render('crafts/index', {
+        h1: "Leonida Crafts",
+        title: "test"
+    });
 });
 
-app.get('/products', function(req, res){
-   res.render('index'); 
+app.get('/darcies', function(req, res){
+    res.render('darcies/index');
 });
+
+app.get('/enchanted', function(req, res){
+    res.render('enchantedteardrops/index');
+});
+
+app.get('/forever', function(req, res){
+    res.render('forever/index');
+});
+
+//app.get('/product/:id', function(req, res){
+//   res.render('view'); 
+//});
+//
+//app.get('/products', function(req, res){
+//   res.render('index'); 
+//});
 
 app.listen(PORT, function(){
    console.log(`App running on port ${PORT}`) 
